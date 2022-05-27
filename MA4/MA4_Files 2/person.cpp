3f15dd1f6dbe2fd41a5e8d7b886f80578a9a6bc0
Ture Hassler <tuha1609@cronstedt.it.uu.se>
2022-05-27 21:26:50 +0200

    Halvvägs med MA4 Del 2, börjat skriva om person.cpp

--- MA4/MA4_Files 2/person.cpp
+++ MA4/MA4_Files 2/person.cpp
@@ -5,6 +5,7 @@ class Person{
 	public:
 		Person(int);
 		int get();
+		int fibc(int);
 		void set(int);
 	private:
 		int age;
@@ -22,10 +23,17 @@ void Person::set(int n){
 	age = n;
 	}
 
+int Person::fibc(int n){
+	if (n <= 1){return 0;}
+	else {return fibc(n-1)+ fibc(n-2);}
+}
+
+
 
 extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
+	int Person_fibc(Person* person, int n) {return person -> fibc(n);}
 	void Person_set(Person* person, int n) {person->set(n);}
 	void Person_delete(Person* person){
 		if (person){
