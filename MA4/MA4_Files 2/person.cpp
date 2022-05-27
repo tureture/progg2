cc5c7669ba7d0d681b3d6a472f6ff80d0fb289aa
Ture Hassler <67737120+tureture@users.noreply.github.com>
2022-05-27 21:39:18 +0200

    Update person.cpp

--- MA4/MA4_Files 2/person.cpp
+++ MA4/MA4_Files 2/person.cpp
@@ -24,8 +24,11 @@ void Person::set(int n){
 	}
 
 int Person::fibc(int n){
-	if (n <= 1){return 0;}
-	else {return fibc(n-1)+ fibc(n-2);}
+	if (n <= 1){
+		return 0;
+	} else {
+		return fibc(n-1) + fibc(n-2);
+	}
 }
 
 
@@ -33,7 +36,7 @@ int Person::fibc(int n){
 extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
-	int Person_fibc(Person* person, int n) {return person -> fibc(n);}
+	int Person_fibc(Person* person, int n) {return person->fibc(n);}
 	void Person_set(Person* person, int n) {person->set(n);}
 	void Person_delete(Person* person){
 		if (person){
